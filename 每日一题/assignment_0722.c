4ae04192637d106313bcaf0d8e997f94a213135d
uosoul <947138750@qq.com>
2023-07-27 14:32:00 +0800

    每日一天0722

---
+++ 每日一题/assignment_0722.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+
+    printf("%lu\n",sizeof(char));
+    printf("%lu\n",sizeof(short));
+    printf("%lu\n",sizeof(int));
+    printf("%lu\n",sizeof(long));
+    printf("%lu\n",sizeof(long long));
+    printf("%lu\n",sizeof(float));
+    printf("%lu\n",sizeof(double));
+    printf("%lu\n",sizeof(short int));
+    printf("%lu\n",sizeof(long int));
+    printf("%lu\n",sizeof(unsigned char));
+    printf("%lu\n",sizeof(unsigned short));
+    printf("%lu\n",sizeof(unsigned long));
+    printf("%lu\n",sizeof(unsigned long long));
+    printf("%lu\n",sizeof(unsigned int));
+
+
+
+
+    return 0;
+
+
+}
